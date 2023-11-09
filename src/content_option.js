bf9b8b3dfae26e28db751bcee26f914622fd0b5e
omkar baliram machpale <97976395+omkarreddy17@users.noreply.github.com>
2023-11-10 02:57:23 +0530

    pro

---
+++ src/content_option.js
@@ -0,0 +1,138 @@
+
+const logotext = "Omkar";
+const meta = {
+    title: "Omkar Machpalle",
+    description: "I’m Aditya pre-final year student, a data enthusiast and a Flutter developer, currently persuing Integrated Mtch degree in CSE spz. Ai & Ml ",
+};
+
+const introdata = {
+    title: "Omkar Machpalle",
+    animated: {
+        first: "I love coding Flutter, Python ",
+        second: "I develop mobile apps",
+        //third: "I develop mobile apps",
+    },
+    description: "I’m Omkar Machpalle pre-final year student, a data enthusiast and a Flutter developer, currently persuing Integrated Mtch degree in CSE spz. Ai & Ml",
+    location : "      VIT Bhopal",
+    home : "      Latur, Maharashtra",
+    // your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "A Undergraduate student in computer science and a passionate developer. Looking out for opportunities for working out with people and have great learning experiences. Love to learn new technologies.",
+};
+const worktimeline = [{
+        jobtitle: "Attendance system with face detection",
+        where: "College Hackathon",
+        date: "2020",
+    },
+    {
+        jobtitle: "Diseaio (a flutter app for detecting diseases by taking symptoms)",
+        where: "Group Project",
+        date: "2022",
+    },
+    {
+        jobtitle: "Expense Tracking App",
+        where: "Personal Project",
+        date: "2023",
+    },
+    {
+        jobtitle: "LeetCode",
+        where: "Competitive Coding",
+        date: "August'23-Present",
+    },
+    {
+        jobtitle: "Participated in Hacktoberfest",
+        where: "Open Source",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "Flutter",
+        value: 90,
+    },
+    {
+        name: "Python",
+        value: 87,
+    },
+    {
+        name: "Pandas",
+        value: 80,
+    },
+    {
+        name: "Java",
+        value: 84,
+    },
+    {
+        name: "Firebase and Firestore",
+        value: 78,
+    },
+    {
+        name: "Flask",
+        value: 50,
+    },
+    {
+        name: "MongoDB",
+        value: 70,
+    },
+    {
+        name: "NodeJS",
+        value: 57,
+    },
+    {
+        name: "PostMan",
+        value: 50,
+    },
+    {
+        name: "MERN Stack",
+        value: 40,
+    },
+];
+
+const services = [{
+        title: "App Developer",
+        description: "As an intermediate Flutter app developer, I am passionate about creating captivating mobile applications that not only function seamlessly but also captivate users with engaging and innovative UI designs. My journey in app development has been a fusion of coding expertise and a deep appreciation for aesthetics.",
+    },
+    {
+        title: "Machine Learning",
+        description: "I am a passionate and aspiring machine learning developer with a keen interest in data processing and analysis, as well as image processing using OpenCV. My journey in the world of technology has been a thrilling adventure, marked by a commitment to harnessing data and machine learning for innovative projects, including those involving the ESP32 platform.",
+    },
+    // {
+    //     title: "Wordpress Design",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
+];
+
+// protfolio 
+
+const contactConfig = {
+    YOUR_EMAIL: "ommachpalle@gmail.com",
+    YOUR_FONE: "+91 9395383616",
+    description: "Happy to connect 😊",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/adityawalture",
+    instagram: "https://www.instagram.com/omkarreddy17",
+    linkedin: "https://www.linkedin.com/in/omkar-machpalle-922309209/",
+    // twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
